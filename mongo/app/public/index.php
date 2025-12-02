33c957a5d744fe9555c3cd951323f398389f0c17
Mattéo <matteo.cadet9@etu.univ-lorraine.fr>
2025-12-02 17:25:54 +0100

    première requete php

--- mongo/app/public/index.php
+++ mongo/app/public/index.php
@@ -13,3 +13,19 @@ require_once __DIR__ . "/../src/vendor/autoload.php";
 $c = new Client("mongodb://mongo");
 echo "connected to mongo <br>";
 
+//liste des produits : numero, categorie, libelle
+$db = $c->pizzashop->produits
+    ->find(
+        [],
+        ['projection'=>
+            ["numero"=>1,
+            "categorie"=>1,
+            "libelle"=>1,]
+        ]
+    );
+
+foreach ($db as $prod) {
+    echo "Numéro : " . (isset($prod["numero"]) ? $prod["numero"] : "") . " | ";
+    echo "Catégorie : " . (isset($prod["categorie"]) ? $prod["categorie"] : "") . " | ";
+    echo "Libellé : " . (isset($prod["libelle"]) ? $prod["libelle"] : "") . "<br>";
+}
