d1b80b103611079e48a82ca6bd14175f725f9d4a
Mattéo <matteo.cadet9@etu.univ-lorraine.fr>
2025-12-09 14:11:51 +0100

    gitignore parce jsp pq le idea se push à nouveau

--- mongo/app/public/index.php
+++ mongo/app/public/index.php
@@ -17,15 +17,44 @@ echo "connected to mongo <br>";
 $db = $c->pizzashop->produits
     ->find(
         [],
-        ['projection'=>
             ["numero"=>1,
             "categorie"=>1,
             "libelle"=>1,]
-        ]
     );
 
 foreach ($db as $prod) {
     echo "Numéro : " . (isset($prod["numero"]) ? $prod["numero"] : "") . " | ";
     echo "Catégorie : " . (isset($prod["categorie"]) ? $prod["categorie"] : "") . " | ";
     echo "Libellé : " . (isset($prod["libelle"]) ? $prod["libelle"] : "") . "<br>";
+}
+
+//liste des produits dont le tarif en taille normale est <= 3.0
+$db = $c->pizzashop->produits->find(
+    ["tarifs"=>[
+        '$elemMatch'=>[
+            "taille"=>"normale",
+            "tarif"=>['$lte' => 3.0]
+        ]
+    ]
+    ],
+    ["projection"=>[
+        "numero"=>1,
+        "categorie"=>1,
+        "libelle"=>1,
+        "tarifs"=>1
+    ]
+    ]
+);
+
+foreach ($db as $prod) {
+    echo "Numéro : " . (isset($prod["numero"]) ? $prod["numero"] : "") . " | ";
+    echo "Catégorie : " . (isset($prod["categorie"]) ? $prod["categorie"] : "") . " | ";
+    echo "Libellé : " . (isset($prod["libelle"]) ? $prod["libelle"] : "") . " | ";
+    if (isset($prod["tarifs"])) {
+        foreach ($prod["tarifs"] as $tarif) {
+            echo "Taille : " . (isset($tarif["taille"]) ? $tarif["taille"] : "") . " - ";
+            echo "Tarif : " . (isset($tarif["tarif"]) ? $tarif["tarif"] : "") . " | ";
+        }
+    }
+    echo "<br>";
 }
